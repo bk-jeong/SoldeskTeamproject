665a8ea62638ad85548e1666c0e686bed10ee713
kmw7691 <101630746+kmw7691@users.noreply.github.com>
2024-02-21 17:08:21 +0900

    0221_todays_final

--- Teamproject/loaproject/loaTier/views.py
+++ Teamproject/loaproject/loaTier/views.py
@@ -1,41 +1,110 @@
 import pandas as pd
 from django.views import View
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from django.forms.models import model_to_dict
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Tier, Raid
 
 # Create your views here.
 
 
+def getToken():
+    egv_init = {
+                "01_bigi": 0,
+                "02_kwangki": 0,
+                "03_boonmang": 0,
+                "04_zoongsu": 0,
+                "05_gogi": 0,
+                "06_zeontae": 0,
+                "07_cheodan": 0,
+                "08_posik": 0,
+                "09_oeganhwa": 0,
+                "10_chosim": 0,
+                "11_zeolzung": 0,
+                "12_zeolze": 0,
+                "13_chesool": 0,
+                "14_choongyuk": 0,
+                "15_yukcheon": 0,
+                "16_semaek": 0,
+                "17_ilgyuk": 0,
+                "18_nanmu": 0,
+                "19_soora": 0,
+                "20_kwaonwang": 0,
+                "21_yoosan": 0,
+                "22_kisool": 0,
+                "23_pogyuk": 0,
+                "24_hwaryuk": 0,
+                "25_doodong": 0,
+                "26_zuksoup": 0,
+                "27_gangmu": 0,
+                "28_hangun": 0,
+                "29_pime": 0,
+                "30_sasi": 0,
+                "31_kyokam": 0,
+                "32_sangso": 0,
+                "33_hwangze": 0,
+                "34_hwanghoo": 0,
+                "35_zeomhwa": 0,
+                "36_hwanryu": 0,
+                "37_choongdong": 0,
+                "38_ukze": 0,
+                "39_burst": 0,
+                "40_zanzae": 0,
+                "41_dalso": 0,
+                "42_galzung": 0,
+                "43_manwol": 0,
+                "44_gumum": 0,
+                "45_zilpoong": 0,
+                "46_islebi": 0,
+                "47_chookoh": 0,
+                "48_simpan": 0,
+                "49_zeolgu": 0,
+                "50_zinyong": 0,
+                "51_mangae": 0,
+                "52_heigwi": 0,
+            }
+    return egv_init
+   
 def index(request):
-    return render(request, "index.html")
+    context = {"engv": list(getToken().keys())}
+    return render(request, "index.html", context)
 
 
 def makeTier(request):
     try:
-        # Insert ORM
-        tier = Tier(
-            tier1=request.POST["1tia"],
-            tier2=request.POST["2tia"],
-            tier3=request.POST["3tia"],
-        )
+        if request.method == "POST":
+            # Insert ORM
+            tier = Tier(
+                tier1=request.POST["1tia"],
+                tier2=request.POST["2tia"],
+                tier3=request.POST["3tia"],
+                tier4=request.POST["4tia"],
+                tier5=request.POST["5tia"],
+                tierout=request.POST["tierout"],
+            )
 
-        # DB save
-        tier.save()
+            # DB save
+            tier.save()
+            return HttpResponseRedirect("/res")
 
         # Select ORM (lastest DB row)
         tierRes = Tier.objects.order_by("-id")[:1]
         tier1 = tierRes[0].tier1.split(",")
         tier2 = tierRes[0].tier2.split(",")
         tier3 = tierRes[0].tier3.split(",")
-
+        tier4 = tierRes[0].tier4.split(",")
+        tier5 = tierRes[0].tier5.split(",")
+        tierout = tierRes[0].tierout.split(",")
+        
         # Returon Dictionary Object
         context = {
             "tier1": tier1,
             "tier2": tier2,
             "tier3": tier3,
+            "tier4": tier4,
+            "tier5": tier5,
+            "tierout": tierout,
         }
+        print(len(tier4))
         return render(request, "userResult.html", context)
     except Exception as e:
         return print(str(e))
@@ -45,12 +114,12 @@ def allResult(request):
     try:
         allDatas = pd.DataFrame(Tier.objects.all().values())
         df = pd.DataFrame(allDatas).loc[
-            :, ["tier1", "tier2", "tier3", "tier4", "tier5"]
+            :, ["tier1", "tier2", "tier3", "tier4", "tier5", ]
         ]
         print(df)
         egv_init = {}
 
-        return_obj = {"tier1": "", "tier2": "", "tier3": "", "tier4": "", "tier5": ""}
+        return_obj = {"tier1": "", "tier2": "", "tier3": "", "tier4": "", "tier5": "", "tierout": ""}
 
         for i in return_obj.keys():
             egv_init = {
