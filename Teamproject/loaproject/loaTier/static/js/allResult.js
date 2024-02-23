8d2e23120b0393234da1ad6076c1da3119a690c8
SoonQchernobyl <113972823+SoonQchernobyl@users.noreply.github.com>
2024-02-23 10:34:04 +0900

    tooltip 구현완료

--- Teamproject/loaproject/loaTier/static/js/allResult.js
+++ Teamproject/loaproject/loaTier/static/js/allResult.js
@@ -5,34 +5,20 @@ window.onload = function(){
 const tooltip_target = document.querySelectorAll(".ebox")
 const tooltip = document.querySelectorAll(".tooltip_image")
 
-// change display to 'block' on mouseover
 
-// function displayblock(element){
-//             element.style.display = 'block';
-// }
-
-// function displaynone(element){
-//     element.style.display = 'none';
-// }
-
-
-
-// for (var index = 0; index < tooltip_target.length; index++) {
-   // this keyword 활용 -> 이벤트를 객체한테만 사용하기
-
-  tooltip_target.addEventListener('mouseover', () => {
-      this.tooltip.style.display = 'block';
-}, true);
+tooltip_target.forEach( function (ebox,index){
+    ebox.addEventListener('mouseover',()=>{
+      tooltip[index].style.display = 'block';
+    })
+  });
 
   // change display to 'none' on mouseleave
-  tooltip_target[index].addEventListener('mouseleave', () => {
-      tooltip[0].style.display = 'none';
+  tooltip_target.forEach(function(ebox,index){
+    ebox.addEventListener('mouseleave', () => {
+      tooltip[index].style.display = 'none';
+    })
   }, false);
-       
-
   
-// }
-
 
 
 
