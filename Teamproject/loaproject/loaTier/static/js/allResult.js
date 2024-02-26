359803e0c344aa6aed3b93710b649ddcd2d0d984
kmw7691 <101630746+kmw7691@users.noreply.github.com>
2024-02-26 09:29:42 +0900

    update_0223

---
+++ Teamproject/loaproject/loaTier/static/js/allResult.js
@@ -0,0 +1,19 @@
+// alert("chack");
+
+window.onload = function () {
+  const tooltip_target = document.querySelectorAll(".ebox");
+  const tooltip = document.querySelectorAll(".tooltip_image");
+
+  tooltip_target.forEach(function (ebox, index) {
+    ebox.addEventListener("mouseover", () => {
+      tooltip[index].style.display = "block";
+    });
+  });
+
+  // change display to 'none' on mouseleave
+  tooltip_target.forEach(function (ebox, index) {
+    ebox.addEventListener("mouseleave", () => {
+      tooltip[index].style.display = "none";
+    });
+  }, false);
+};
